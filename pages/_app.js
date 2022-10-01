2aa86132473d2acf34fb9d4470d49c6fef7edf10
dhian haryono <dhianharyono.dh@gmail.com>
2022-10-01 08:56:22 +0700

    add hotjar tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(3180428, 6)
+  }, [])
+return <Component {...pageProps} />
 }
-
 export default MyApp
